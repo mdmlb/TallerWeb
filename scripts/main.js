0309f3c559f0db6a81aacff5f48c240969ade87a
mdmlb <mlopez0195@gmail.com>
2020-09-22 17:44:51 -0500

    charact interaction almost done, problems with the ocapity of texts

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,35 @@
 
+//Charact
+const charact = document.querySelector('.charact');
+const charactText1 = document.querySelector('.charact__textOne');
+const charactText2 = document.querySelector('.charact__textTwo');
+const charactText3 = document.querySelector('.charact__textThree');
+const charactDot1 = document.querySelector('.charact__dotOne');
+const charactDot2 = document.querySelector('.charact__dotTwo');
+const charactDot3 = document.querySelector('.charact__dotThree');
 
+charactDot1.addEventListener('click', function(){
+    //let scr = "./Resources/dotActive.png";
+    charactDot1.setAttribute('src', './Resources/dotActive.png');
+    charactText1.style.transform = 'opacity(none)';
+    
+    console.log();
+});
+
+charactDot2.addEventListener('click', function(){
+    charactDot2.setAttribute('src', './Resources/dotActive.png');
+
+    console.log("Boton 2");
+});
+
+charactDot3.addEventListener('click', function(){
+    charactDot3.setAttribute('src', './Resources/dotActive.png');
+    console.log("Boton 3");
+});
+
+
+
+//Gallery
 const carrousel = document.querySelector('.carrousel');
 const carrouselStripe = document.querySelector('.carrousel__stripe');
 const carrouselSlider = document.querySelector('.carrousel__slider');
